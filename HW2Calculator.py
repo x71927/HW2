856410830f0d0d82977382582eab1c2ffafacb26
Ted <x71927@gmail.com>
2017-02-08 14:17:05 -0600

    commit 2: Subtraction unit written and tested

--- HW2Calculator.py
+++ HW2Calculator.py
@@ -1,6 +1,8 @@
 import sys
 def Adder(a, b):
     return a + b
+def Subtracter(a,b):
+    return a - b
 def test():
     if 3 == Adder(1,2):
         print("positive integer test passed")
@@ -18,6 +20,22 @@ def test():
         print("zero addition passed")
     else:
         print("zero addition failed")
+    if 3 == Subtracter(6,3):
+        print("positive subtraction passed")
+    else:
+        print("positive subtraction failed")
+    if 5 == Subtracter(3,-2):
+        print("negative and positive subtraction test passed")
+    else:
+        print("negative and positive subtraction test failed")
+    if -1 == Subtracter(-3,-2):
+        print("negative subtration passed")
+    else:
+        print("negative subtraction failed")
+    if 1 == Subtracter(1,0):
+        print("zero subtraction passed")
+    else:
+        print("zero subtraction passed")
 def main():
     test()
 
